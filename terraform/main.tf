2d6fea49580a66f2adf288982ec424b02a052e82
Akhil Nath Reddy <akhilkommuru98@gmail.com>
2026-05-03 21:12:14 +0530

    feat(terraform): secure S3 bucket provisioning

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "shopsmart-bucket-${random_id.bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_bucket_encryption" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
